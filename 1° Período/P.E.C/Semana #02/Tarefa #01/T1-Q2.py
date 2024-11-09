d00a213907612ac9e4d69af2c2d8e671f5a6f1b8
Luiz Eduardo Gomes <160649342+LuizEduardo20@users.noreply.github.com>
2024-11-09 10:30:08 -0300

    Add files via upload

---
+++ 1° Período/P.E.C/Semana #02/Tarefa #01/T1-Q2.py
@@ -0,0 +1,6 @@
+n = str(input()).strip()
+nome = n.strip()
+s= str(input()).strip()
+Sobrenome = s.strip()
+nome_completo = nome + " " + Sobrenome
+print(nome_completo)
---
+++ 1° Período/P.E.C/Semana #02/Tarefa #01/T1-Q3.py
@@ -0,0 +1,8 @@
+numero1 = float(input().strip())
+numero2 = float(input().strip())
+
+soma = numero1 + numero2
+multiplicacao = numero1 * numero2
+
+print(soma)
+print(multiplicacao)
