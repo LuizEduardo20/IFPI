c70084bfcb79d6180d9ac386049749674212cb08
Luiz Eduardo Gomes <160649342+LuizEduardo20@users.noreply.github.com>
2024-11-10 19:44:36 -0300

    Update T2-Q5.py

--- 1° Período/P.E.C/Semana #06/Tarefa #02/T2-Q5.py
+++ 1° Período/P.E.C/Semana #06/Tarefa #02/T2-Q5.py
@@ -1,7 +1,3 @@
-# Você sabia que os pinguins usam jaquetas devido ao frio na Antártida? Vamos ajudá-los a converter temperaturas!
-# Escreva um programa que leia uma temperatura em Celsius e mostre o resultado em Fahrenheit. Lembre-se:
-# Fahrenheit = (Celsius x (9 / 5)) + 32
-
 def convert_(temperatura):
     return (temperatura * (9 / 5)) + 32
 
