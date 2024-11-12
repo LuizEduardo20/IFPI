26c042c9fe6ba1d076f1c09adb4a0c1319c7a967
Luiz Eduardo Gomes <160649342+LuizEduardo20@users.noreply.github.com>
2024-11-12 08:01:30 -0300

    Update T1-Q4.py

--- 1° Período/P.E.C/Semana #16/T1-Q4.py
+++ 1° Período/P.E.C/Semana #16/T1-Q4.py
@@ -1,19 +1,4 @@
-# A tabela abaixo demonstra a quantidade de vendas dos fabricantes de veículos durante o período de 2013 a 2018, em mil unidades.
-
-# Fabricante / Ano	2013	2014	2015	2016	2017	2018
-# Fiat	            204	    223	    230	    257	    290 	322
-# Ford	            195 	192 	198 	203 	208 	228
-# GM	            220 	222 	217 	231 	245 	280
-# Wolkswagen	    254 	262 	270 	284 	296 	330
-
-# Faça um programa em python que:
-
-# leia os dados da tabela pelo teclado;
-# leia um ano do período determine e exiba o fabricante que mais vendeu nesse ano;
-# determine e exiba o ano de maior volume geral de vendas.
-# determine e exiba a média anual de vendas de cada fabricante durante o período.
-
-def valores_matriz(n=4, m=6): # seria os dados da tabela pra formar a matriz?
+def valores_matriz(n=4, m=6):
     matriz = []
     for i in range(n):
         linha = []
