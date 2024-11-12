08180d3d81732d0cd5a56425ccaaada37f6afeda
Luiz Eduardo Gomes <160649342+LuizEduardo20@users.noreply.github.com>
2024-11-12 08:00:46 -0300

    Update T1-Q3.py

--- 1° Período/P.E.C/Semana #16/T1-Q3.py
+++ 1° Período/P.E.C/Semana #16/T1-Q3.py
@@ -8,25 +8,21 @@ def valores(n, m):
         matriz.append(linha)
     return matriz
 
-# a) a soma dos elementos da primeira linha ---- OK
 def soma_primeiraLinha(matriz):
     return sum(matriz[0])
 
-# b) a soma dos elementos da última coluna ---- OK
 def soma_ultimaColuna(matriz):
     soma = 0
-    for i in matriz: # i é linha, não um indice pq está iterando diretamente na matriz e nao no tamanho dela usando len.
+    for i in matriz:
         soma += i[-1]
     return soma
 
-# c) a média de todos os elementos ---- OK
 def media_todosElementos(matriz, n, m):
     soma = 0   
     for i in matriz:
         soma += sum(i)         
     return round(soma / (n * m), 4)
 
-# d) o menor elemento ---- OK
 def menor_Elemento(matriz):
     menor = matriz[0][0]
     for i in range(len(matriz)):
@@ -35,7 +31,6 @@ def menor_Elemento(matriz):
                 menor = matriz[i][j]
     return menor
 
-# e) o maior elemento ---- OK
 def maior_Elemento(matriz):
     maior = matriz[0][0]
     for i in range(len(matriz)):
