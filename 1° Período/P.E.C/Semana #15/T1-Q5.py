0b9dc9a20814d5d41a975e43b73a6b2d991f61f3
Luiz Eduardo Gomes <160649342+LuizEduardo20@users.noreply.github.com>
2024-11-12 07:58:25 -0300

    Update T1-Q5.py

--- 1° Período/P.E.C/Semana #15/T1-Q5.py
+++ 1° Período/P.E.C/Semana #15/T1-Q5.py
@@ -1,5 +1,3 @@
-
-
 def carrega_cidades():
     resultado = []
     with open('cidades.csv', 'r', encoding='utf-8') as arquivo:
@@ -15,17 +13,16 @@ def populacao_maior_aniversario(mes, populacao, cidades):
     lista_cidades_encontradas = []
     for cidade in cidades:
         if cidade[5] > populacao and cidade[4] == mes:
-            # Penedo(AL) tem 59020 habitantes e faz aniversário em 12 de abril.
             lista_cidades_encontradas.append(f'{cidade[2]}({cidade[0]}) tem {cidade[5]} habitantes e faz aniversário em {cidade[3]} de {mes_por_extenso(mes).lower()}.')          
     return lista_cidades_encontradas
 
 def mes_por_extenso(mes):
     meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
     
-    return meses[mes - 1] # ex: se mes 1, o indice será 1 - 1 = 0. 
+    return meses[mes - 1] 
 
 def main():
-    cidades = carrega_cidades() # tem a lista de todas as cidades com as informações
+    cidades = carrega_cidades()
     
     mes = int(input())
     populacao = int(input())
