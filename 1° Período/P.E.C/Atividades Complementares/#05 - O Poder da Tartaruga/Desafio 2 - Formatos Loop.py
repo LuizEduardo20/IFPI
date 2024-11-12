aa40e054c0d26121d6a90967e5e7d12d30375963
Luiz Eduardo Gomes <160649342+LuizEduardo20@users.noreply.github.com>
2024-11-12 15:55:29 -0300

    Update Desafio 2 - Formatos Loop.py

--- 1° Período/P.E.C/Atividades Complementares/#05 - O Poder da Tartaruga/Desafio 2 - Formatos Loop.py
+++ 1° Período/P.E.C/Atividades Complementares/#05 - O Poder da Tartaruga/Desafio 2 - Formatos Loop.py
@@ -3,12 +3,9 @@ from turtle import *
 speed(10)
 shape("turtle")
 
-# Pentágono
 for count in range(5):
     forward(50)
     right(72)
-    
-# Hexágono
 
 penup()
 forward(100)
@@ -16,8 +13,6 @@ pendown()
 for count in range(6):
     forward(50)
     right(60)
-
-# Circulo 
 
 penup()
 left(90)
