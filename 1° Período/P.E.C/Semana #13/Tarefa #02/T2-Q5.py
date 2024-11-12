bd99a660a63eb352403ce3b4fc9ac5b598b5f49d
Luiz Eduardo Gomes <160649342+LuizEduardo20@users.noreply.github.com>
2024-11-12 07:46:27 -0300

    Update T2-Q5.py

--- 1° Período/P.E.C/Semana #13/Tarefa #02/T2-Q5.py
+++ 1° Período/P.E.C/Semana #13/Tarefa #02/T2-Q5.py
@@ -17,7 +17,7 @@ def esta_ordenada(lista):
     for i in range(len(lista) - 1):
         if lista[i] > lista[i + 1]:
             return False
-    return True # Só False se pelo menos um for menor que o próximo, True: valor PADRÃO
+    return True
 
 def main():
     n = int(input())
