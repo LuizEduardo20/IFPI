2153164319d3159c97f8045ebc6620b82b1e2663
Luiz Eduardo Gomes <160649342+LuizEduardo20@users.noreply.github.com>
2025-03-25 17:11:31 -0300

    Update Q1.py

--- 3° Período/P.O.T/Atividade - 01/Q1.py
+++ 3° Período/P.O.T/Atividade - 01/Q1.py
@@ -1,10 +1,17 @@
-numero = int(input('Digita um valor legal ai!!'))
 
 def verificar_numero(numero):
     if numero % 2 == 0:
         return 'par'
+    
     else:
         return 'ímpar'
+
+while True:
+    try:
+        numero = int(input('Digite um valor inteiro: '))
+        break
+    except ValueError:
+        print('Erro: Por favor, digite apenas números inteiros!')
 
 resultado = verificar_numero(numero)
 
