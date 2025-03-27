cd4fadc420425c4232df4db6e3162dba48d1840a
Luiz Eduardo Gomes <160649342+LuizEduardo20@users.noreply.github.com>
2025-03-27 08:36:07 -0300

    Create Q4.py

---
+++ 3° Período/P.O.T/Atividade - 01/Q4.py
@@ -0,0 +1,18 @@
+def somarMedia(n1, n2):
+  if 6 < (n1 + n2) / 2:
+    print('Aprovado')
+
+  else:
+    print('Reprovado')
+
+while True:
+  try:
+    nota1 = float(input('Digite a primeira nota: ')
+    nota2 = float(input('Digite a segunda nota: ')
+    break
+
+  except ValueError:
+    print('Valores Invalidos')
+
+resultado = somarMedia(nota1, nota2)
+print(resultado)
