d6111218f412998e92127f9449bd4eaf4ab7ab60
Luiz Eduardo Gomes <160649342+LuizEduardo20@users.noreply.github.com>
2025-03-28 15:32:26 -0300

    Update Q14.py

--- 3° Período/P.O.T/Atividade - 01/Q14.py
+++ 3° Período/P.O.T/Atividade - 01/Q14.py
@@ -1,10 +1,15 @@
+def fatorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * fatorial(n-1)
+
 def somatoria(n):
     if n <= 0:
         return "Por favor, insira um número positivo"
     
     s = 0
-    for i in range(1, n + 1):
-        s += 1/i
+    for i in range(n + 1):
+        s += 1/fatorial(i)
     return s
 
 while True:
