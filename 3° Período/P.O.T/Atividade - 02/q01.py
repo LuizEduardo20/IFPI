e8beffbc6b695f445f15772686dbab133a370106
Luiz Eduardo Gomes <160649342+LuizEduardo20@users.noreply.github.com>
2025-04-03 14:53:23 -0300

    Update q01.py

--- 3° Período/P.O.T/Atividade - 02/q01.py
+++ 3° Período/P.O.T/Atividade - 02/q01.py
@@ -1,1 +1,22 @@
+lista = []
+listaPar = []
+listaImpar = []
 
+for i in range(100):
+    while True:
+        try:
+            valor = int(input("Digite um valor: "))
+            lista.append(valor)
+            
+            if valor % 2 == 0:
+                listaPar.append(valor)
+            else:
+                listaImpar.append(valor)
+                break
+            
+        except ValueError:
+            print("Valor invalido")
+
+print(lista)
+print(listaPar)
+print(listaImpar)
