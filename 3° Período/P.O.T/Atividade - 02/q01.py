2e206a99d07d8b63c3b3b2f6aea785f9d4df879a
Luiz Eduardo Gomes <160649342+LuizEduardo20@users.noreply.github.com>
2025-04-09 16:49:02 -0300

    Update q01.py

--- 3° Período/P.O.T/Atividade - 02/q01.py
+++ 3° Período/P.O.T/Atividade - 02/q01.py
@@ -2,21 +2,25 @@ lista = []
 listaPar = []
 listaImpar = []
 
-for i in range(100):
+for i in range(5):
     while True:
         try:
-            valor = int(input("Digite um valor: "))
-            lista.append(valor)
-            
-            if valor % 2 == 0:
-                listaPar.append(valor)
-            else:
-                listaImpar.append(valor)
-                break
+            x = int(input("Digite um valor: "))
+            lista.append(x)
+            break
             
         except ValueError:
             print("Valor invalido")
 
+def impa_par(valor):
+    if valor % 2 == 0:
+        listaPar.append(valor)
+    else:
+        listaImpar.append(valor)
+for i in range(len(lista)):       
+    impa_par(lista[i])
+
 print(lista)
-print(listaPar)
 print(listaImpar)
+print(listaPar)
+
