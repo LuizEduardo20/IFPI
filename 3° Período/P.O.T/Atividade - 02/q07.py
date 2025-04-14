fd7520c9dde60617db324022f615baad4478ba64
Luiz Eduardo Gomes <160649342+LuizEduardo20@users.noreply.github.com>
2025-04-14 14:24:30 -0300

    Update q07.py

--- 3° Período/P.O.T/Atividade - 02/q07.py
+++ 3° Período/P.O.T/Atividade - 02/q07.py
@@ -1,18 +1,20 @@
-lista = []
+def inserir_numeros_na_lista():
+    lista = []
+    for i in range(10):
+        while True:
+            try:
+                numeros = int(input(f"Digite o {i+1}º número: "))
 
-for i in range(10):
-    while True:
-        try:
-            numeros = int(input(f"Digite o {i+1}º número: "))
+                if numeros in lista:
+                    print('Número já existe na lista')
+                else:
+                    lista.append(numeros)
+                    break
 
-            if numeros in lista:
-                print('Número já existe na lista')
-                
-            else:
-                lista.append(numeros)
-                break
+            except ValueError:
+                print('Erro!! Digite apenas números inteiros')
+    return lista
 
-        except ValueError:
-            print('Erro!! Digite apenas números inteiros')
+lista = inserir_numeros_na_lista()
 
 print(lista)
