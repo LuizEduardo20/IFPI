877c7ef6d38584d7a29afd5d8598a17fca0cb64f
Luiz Eduardo Gomes <160649342+LuizEduardo20@users.noreply.github.com>
2025-04-14 14:26:40 -0300

    Update q09.py

--- 3° Período/P.O.T/Atividade - 02/q09.py
+++ 3° Período/P.O.T/Atividade - 02/q09.py
@@ -1,14 +1,20 @@
-X = []
+def inserir_numeros():
+    X = []
+    for i in range(5):
+        while True:
+            try:
+                numero = int(input(f"Digite o {i+1}º número: "))
+                X.append(numero)
+                break
+            except ValueError:
+                print("Erro: Por favor, digite apenas números válidos!")
+    return X
 
-while True:
-    try:
-        for i in range(5):
-            numero = int(input(f"Digite o {i+1}º número: "))
-            X.append(numero)
-            Y = X[::-1]
-            
-        print("\nLista X:", X)
-        print("Lista Y:", Y)
-            
-    except ValueError:
-        print("Erro: Por favor, digite apenas números válidos!")
+def inverter_lista(lista):
+    return lista[::-1]
+
+X = inserir_numeros()
+Y = inverter_lista(X)
+
+print("\nLista X:", X)
+print("Lista Y:", Y)
