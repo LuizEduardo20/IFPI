d278a6bc286aa18fa8935a1304612c8c24a55c7f
Luiz Eduardo Gomes <160649342+LuizEduardo20@users.noreply.github.com>
2025-04-14 14:25:30 -0300

    Update q08.py

--- 3° Período/P.O.T/Atividade - 02/q08.py
+++ 3° Período/P.O.T/Atividade - 02/q08.py
@@ -1,30 +1,35 @@
-alfabeto = []
+def inserir_letras_alfabeto():
+    alfabeto = []
+    print("Digite 10 letras do alfabeto:")
+    for i in range(10):
+        while True:
+            try:
+                letra = input(f"Digite a {i+1}ª letra: ").strip().upper()
 
-print("Digite 10 letras do alfabeto:")
-for i in range(10):
-    while True:
-        try:
-            letra = input(f"Digite a {i+1}ª letra: ").strip().upper()
-            
-            if len(letra) != 1:
-                print("Erro! Digite apenas uma letra.")
-                continue
-                
-            if not letra.isalpha():
+                if len(letra) != 1:
+                    print("Erro! Digite apenas uma letra.")
+                    continue
+
+                if not letra.isalpha():
+                    print("Erro! Digite apenas letras do alfabeto.")
+                    continue
+
+                alfabeto.append(letra)
+                break
+
+            except Exception:
                 print("Erro! Digite apenas letras do alfabeto.")
-                continue
-                
-            alfabeto.append(letra)
-            break
-            
-        except Exception:
-            print("Erro! Digite apenas letras do alfabeto.")
+    return alfabeto
 
-contador = 0
+def contar_letra(alfabeto, letra_busca):
+    contador = 0
+    for letra in alfabeto:
+        if letra == letra_busca:
+            contador += 1
+    return contador
 
-for letra in alfabeto:
-    if letra == 'A':
-        contador += 1
+alfabeto = inserir_letras_alfabeto()
+contador_A = contar_letra(alfabeto, 'A')
 
 print("\nLista de letras digitadas:", alfabeto)
-print(f"A letra 'A' aparece {contador} vezes na lista.")
+print(f"A letra 'A' aparece {contador_A} vezes na lista.")
