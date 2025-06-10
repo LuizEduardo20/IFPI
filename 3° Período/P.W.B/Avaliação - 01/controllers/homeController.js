db935c51b9ed76bcd41fb190ad92dc840c00bcbb
Luiz Eduardo <160649342+LuizEduardo20@users.noreply.github.com>
2025-06-10 14:29:55 -0300

    Delete 3° Período/P.W.B/Avaliação - 01 directory

--- 3° Período/P.W.B/Avaliação - 01/controllers/homeController.js
+++
@@ -1,4 +0,0 @@
-exports.paginaInicial = (req, res) => {
-    res.render('pages/home', { css: 'home' });
-};
-  
--- 3° Período/P.W.B/Avaliação - 01/routes/jogo.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const jogoController = require('../controllers/jogoController');
-
-router.get('/', jogoController.telaJogo);
-router.post('/responder', jogoController.responder);
-router.get('/continuar', jogoController.continuar);
-
-module.exports = router;
