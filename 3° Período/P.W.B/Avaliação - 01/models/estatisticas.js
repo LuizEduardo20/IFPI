fc0efab65d6d3167b17994be10fda975cd8d770a
Luiz Eduardo <160649342+LuizEduardo20@users.noreply.github.com>
2025-06-11 14:33:04 -0300

    Update estatisticas.js

--- 3° Período/P.W.B/Avaliação - 01/models/estatisticas.js
+++ 3° Período/P.W.B/Avaliação - 01/models/estatisticas.js
@@ -1,16 +1,13 @@
-// Objeto para armazenar as estatísticas
 let estatisticas = {
   totalRespondidas: 0,
   acertos: 0,
   erros: 0
 }
 
-// Função para obter as estatísticas
 function getEstatisticas() {
   return estatisticas
 }
 
-// Função para atualizar as estatísticas
 function atualizarEstatisticas(acertou) {
   estatisticas.totalRespondidas++
   if (acertou) {
@@ -20,7 +17,6 @@ function atualizarEstatisticas(acertou) {
   }
 }
 
-// Função para reiniciar as estatísticas
 function reiniciarEstatisticas() {
   estatisticas = {
     totalRespondidas: 0,
